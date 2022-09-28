6c2211f9f52824e719a272cea6110b21bdeec900
Changeun An <eun091696@gmail.com>
2022-09-28 22:50:40 +0900

    22.09.28 IntelliJ 2일차 기본값 설정 및 @GetMapping을 사용하여 주소창 값 화면 출력

---
+++ mvc-20220927-changeun/src/main/java/com/study/mvc20220927changeun/Mvc20220927ChangeunApplication.java
@@ -0,0 +1,13 @@
+package com.study.mvc20220927changeun;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Mvc20220927ChangeunApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Mvc20220927ChangeunApplication.class, args);
+	}
+
+}
