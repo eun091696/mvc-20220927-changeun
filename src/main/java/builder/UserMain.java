b393696f901a8a4522f20a7a3b907e0c2eee3ddf
eun091696 <eun091696@gmail.com>
2022-10-14 17:39:55 +0900

    no message

---
+++ src/main/java/builder/UserMain.java
@@ -0,0 +1,10 @@
+package builder;
+
+public class UserMain {
+    public static void main(String[] args) {
+        User user = User.UserBuilder.UserBuilder()
+                        .username("junil")
+                        .password("1234")
+                        .build();
+    }
+}
